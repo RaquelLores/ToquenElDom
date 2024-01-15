1e8de10cbc2ee2ee1b507e7f246ba95809651f61
TrianaSolerMartin <triana287@gmail.com>
2024-01-15 14:30:06 +0100

    adding navar y header to feature/piano

--- Src/index.js
+++ Src/index.js
@@ -38,27 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
 /*aqui trminan los script de page_under_construction*/
 /* Aquí empiezan los estilos JS del teclado */
 
-// Selecciona todos los elementos con la clase "teclas_blancas"
-//let teclasBlancas = document.querySelectorAll('.teclas_blancas');
 
-// Realiza alguna acción con cada elemento seleccionado
-//teclasBlancas.forEach(teclaBlanca => {  teclaBlanca.style.backgroundColor = 'yellow';});
 
-// Selección del elemento HTML
-const imagenElement = document.getElementById('imagenCambiante');
 
-// Definición de las imágenes a cambiar
-const imagenes = ['imagen1.jpg', 'imagen2.jpg', 'imagen3.jpg'];
-let indiceImagenActual = 0;
 
-// Creación de la función principal
-function cambiarImagen() {
-    // Alternancia entre las imágenes
-    indiceImagenActual = (indiceImagenActual + 1) % imagenes.length;
-
-    // Manipulación del atributo 'src' para cambiar la imagen
-    imagenElement.src = imagenes[indiceImagenActual];
-}
-
-// Uso del evento 'click' para llamar a la función al hacer clic
-imagenElement.addEventListener('click', cambiarImagen);
