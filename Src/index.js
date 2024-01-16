86cffe99622ae978cf375e0b60bd478a00e57031
RaquelLores <128092239+RaquelLores@users.noreply.github.com>
2024-01-16 10:37:06 +0100

    formulario con checkbox dinámico

--- Src/index.js
+++ Src/index.js
@@ -86,5 +86,16 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 
+  //JS de la página de contacto
+  function enviarFormulario() {
+    var checkbox = document.getElementById('gridCheck');
+
+    if (checkbox.checked) {
+        window.alert('Tu mensaje se ha enviado.');
+    } else {
+        window.alert('Por favor, acepta la política de privacidad haciendo clic en la casilla.');
+    }
+}
+
 
 
