7e385a6c4916b8c08e35d2feef84c11e2c32f3e1
TrianaSolerMartin <triana287@gmail.com>
2024-01-16 12:11:20 +0100

    adding new function, id, and js to piano to play the sound in the whitekeys

--- Src/index.js
+++ Src/index.js
@@ -85,5 +85,48 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 
+//-----SONIDO-------//
 
+//Do
+ 
+ // 3. Función de reproducción de audio
+ function reproducirAudio(id) {
+    let audio = document.getElementById(id);
+    audio.play();
+  }
+  // 4. Código JavaScript
+  const tecla_blancado = document.getElementById("dotecla_blanca")
+  tecla_blancado.addEventListener("click",function(){
+    return reproducirAudio("audiodo")
+  });
 
+  //RE
+  const tecla_blancare = document.getElementById("retecla_blanca")
+  tecla_blancare.addEventListener("click",function(){
+    return reproducirAudio("audiore")
+  });
+
+  const tecla_blancami = document.getElementById("mitecla_blanca")
+  tecla_blancami.addEventListener("click",function(){
+    return reproducirAudio("audiomi")
+  });
+
+  const tecla_blancafa = document.getElementById("fatecla_blanca")
+  tecla_blancafa.addEventListener("click",function(){
+    return reproducirAudio("audiofa")
+  });
+
+  const tecla_blancasol = document.getElementById("soltecla_blanca")
+  tecla_blancasol.addEventListener("click",function(){
+    return reproducirAudio("audiosol")
+  });
+
+  const tecla_blancala = document.getElementById("latecla_blanca")
+  tecla_blancala.addEventListener("click",function(){
+    return reproducirAudio("audiola")
+  });
+
+  const tecla_blancasi = document.getElementById("sitecla_blanca")
+  tecla_blancasi.addEventListener("click",function(){
+    return reproducirAudio("audiosi")
+  });
